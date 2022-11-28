5ce557132923f00f5451fda6c4263b3fd6f1f59a
DoniyorShodiyevDoni <109288985+Doniyor2555@users.noreply.github.com>
2022-11-28 17:58:13 +0500

    Upload file

--- src/question.js
+++ src/question.js
@@ -9,8 +9,19 @@ export class Question {
     })
     .then(response => response.json())
     .then(response => {
-      console.log(response);
+      question.id = response.name;
+      return question;
     })
+    .then(addToLocalStorage);
   }
 }
 
+function addToLocalStorage(question){
+  const all = getQuestionsFromLocalStorage();
+  all.push(question);
+  localStorage.setItem("question", JSON.stringify(all));
+}
+
+function getQuestionsFromLocalStorage(){
+  return JSON.parse(localStorage.getItem("question") || '[]'); 
+}
