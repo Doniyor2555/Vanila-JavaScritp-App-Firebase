788a1caa2314b8b9e875e4d7969f40a970b4df0d
DoniyorShodiyevDoni <109288985+Doniyor2555@users.noreply.github.com>
2022-11-29 15:05:15 +0500

    Upload file

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,8 @@ export function isValid(value){
   return value.length >= 10;
 }
 
-export function createModal(title, html){
-  
+export function createModal(title, content){
+  const modal = document.createElement("div");
+
+  mui.overlay("on", modal)
 }
